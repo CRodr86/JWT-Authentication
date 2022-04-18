ce5dac413d0f0a39672256e228eae98ebb5abb77
CRodr86 <rodrigocfotografia@gmail.com>
2022-04-18 22:16:31 +0000

    stored token and added login/logout button

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -7,41 +8,34 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const token = sessionStorage.getItem("token");
+    const history = useHistory();
+    console.log("This is your token", store.token)
 
     const handleClick = () => {
-        const opts = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "email": email,
-                "password": password
-            })
-        }
-        fetch('https://3001-crodr86-jwtauthenticati-fbdd0lorso9.ws-eu40.gitpod.io/api/token', opts)
-        .then(resp => {
-            if(resp.status === 200) return resp.json();
-            else alert("Error!")
-        })
-        .then()
-        .catch(error => {
-            console.error("There was an error!", error);
-        })
-    }
+        actions.login(email, password);
+    };
+
+    if(store.token && store.token != "" && store.token != undefined) history.push("/");
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Log in</h1>
-			<div className="row">
-                <div className="col-12 my-3">
-                    <input className="col-3" type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            {(store.token && store.token != "" && store.token != undefined) ? (
+                "You are logged in" + store.token
+                ) : (
+            <>
+                <div className="row">
+                    <div className="col-12 my-3">
+                        <input className="col-3" type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="col-12 mb-3">                   
+                        <input className="col-3" type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
                 </div>
-                <div className="col-12 mb-3">                   
-                    <input className="col-3" type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-            </div>
-            <button onClick={handleClick} type="button" className="btn btn-primary">Log in</button>
+                <button onClick={handleClick} type="button" className="btn btn-primary">Log in</button>
+            </>
+            )}
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -19,6 +20,48 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
+			},
+
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("Application just loaded")
+				if(token && token != "" && token != undefined) setStore({ token: token });
+			},
+
+			logout: () => {
+				sessionStorage.removeItem("token");
+				console.log("Logging out");
+				setStore({ token: null });
+			},
+
+			login: async (email, password) => {
+				const opts = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+				
+				try {
+					const resp = await fetch('https://3001-crodr86-jwtauthenticati-fbdd0lorso9.ws-eu40.gitpod.io/api/token', opts)
+					if(resp.status !== 200) {
+						alert("Error!");
+						return false;
+					}
+
+					const data = await resp.json();
+					console.log("This came from the backend", data);
+					sessionStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token });
+					return true;
+				}
+				catch(error) {
+					console.error("There has been an log in error");
+				}
 			},
 
 			getMessage: () => {
